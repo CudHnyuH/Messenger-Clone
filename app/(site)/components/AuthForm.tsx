407ae8e95499080490120a6fc4b4622e7072a568
cudhnyuh <phamhuynhduc805@gmail.com>
2023-06-18 19:42:51 +0700

    NextAuth Setup

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { useCallback, useState } from "react";
 import { BsGithub, BsGoogle  } from 'react-icons/bs';
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
+import axios from "axios";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -39,7 +40,7 @@ const AuthForm = () => {
         setIsLoading(true);
 
         if (variant === 'REGISTER') {
-
+            axios.post('/api/register', data)
         }
 
         if (variant === 'LOGIN') {
